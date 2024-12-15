7b3144adf98b364458ba48953455f53907b9777b
Bert Denayer <bert.denayer@student.howest.be>
2024-12-15 10:06:50 +0100

    setup repo with docs and api

---
+++ js/pokemon.js
@@ -0,0 +1,16 @@
+`use strict`
+
+
+window.addEventListener('load', initialise);
+
+// variabelen
+
+// DOM variabelen
+
+// arrays
+
+// json source
+
+function initialise(){
+    console.log(pokemon);
+}
